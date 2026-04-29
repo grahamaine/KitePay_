4a897407f0deda1423a0f604d0a90fe859670416
Graham Byagaba <ainegb05@gmail.com>
2026-04-29 14:23:29 +0400

    final twerks

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,18 +5,29 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory
+    .dir("../../build")
+    .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+    
+    // THE 2026 NAMESPACE FIX:
+    // This automatically fixes "Namespace not specified" errors in your Flutter plugins
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace == null) {
+                android.namespace = "com.kitepay.generated.${project.name.replace("-", "_")}"
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
